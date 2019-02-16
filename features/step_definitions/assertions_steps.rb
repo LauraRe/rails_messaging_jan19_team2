9beb2191686208e91179f0f9ebd7e6ab8f70b84b
Laura Reale <laaru1988@gmail.com>
2019-02-16 14:42:54 +0100

    add sad path to send message feature

--- features/step_definitions/assertions_steps.rb
+++ features/step_definitions/assertions_steps.rb
@@ -2,6 +2,6 @@ Then("I should see {string}") do |expected_content|
     expect(page).to have_content expected_content
 end
 
-Then("I should select {string} from {string}") do |recipient, container|
+Then("I select {string} from {string}") do |recipient, container|
    select recipient, from: container
 end
